ce623a52c1aaa54769e734698030b921b933f772
omarsilverio <omar.silverio.98@gmail.com>
2024-10-07 20:26:50 -0600

    add  boostrap 5.3.3

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -15,6 +15,9 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
     return (
         <>
             <Head title="Bienvenido" />
+            <button className="btn btn-success">
+                hola
+            </button>
             <div className="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
                 <img
                     id="background"
--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,4 +1,5 @@
 import '../css/app.css';
+import '../sass/app.scss';
 import './bootstrap';
 
 import { createInertiaApp } from '@inertiajs/react';
